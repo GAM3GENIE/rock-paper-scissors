060ee8fe479f8c80ff740c37cd6b96b07280d33a
GAM3GENIE <davidlbernardino@gmail.com>
2022-02-19 12:24:32 -0800

    creating game function

--- index.js
+++ index.js
@@ -1,42 +1,78 @@
-function computerPlay(compTurn){
-    return compTurn[Math.floor(Math.random() * compTurn.length)];
-}
+const playGame = () => {
+let roundMessage = '';
+let playerScore = 0;
+let computerScore = 0;
+let playerSelection = '';
 
-const compTurn = ['rock', 'paper', 'scissors'];
-//console.log(computerPlay(compTurn))
+    function computerPlay(compTurn){
+        return compTurn[Math.floor(Math.random() * compTurn.length)];
+    }
 
+    const compTurn = ['rock', 'paper', 'scissors'];
+    //console.log(computerPlay(compTurn))
+    const computerSelection = computerPlay(compTurn);
 
-function playRound(playerSelection){
-    if(playerSelection == computerSelection){
-        results = 'Tie';
-    }else if(playerSelection = 'rock'){
-        if(computerSelection = 'paper'){
-            results = 'You lose! Paper beats Rock!';
-            console.log(results)
-        }else{
-            results = 'You won! Rock was the best choice!';
-            console.log(results)
-        }
-    }else if(playerSelection = 'scissors'){
-        if(computerSelection = 'rock'){
-            results = 'You lost! Rock beats Scissors!'
-            console.log(results)
-        }else{
-            results = 'You won! Scissors was the best choice!';
-            console.log(results)
-        }
-    }else if(playerSelection = 'paper'){
-        if(computerSelection = 'scissors'){
-            results = 'You lose! Scissors beats Paper!';
-            console.log(results)
-        }else{
-            results = 'You won! Paper was the best choice!';
-            console.log(results)
+    function playRound(playerSelection, computerSelection){
+        if(playerSelection === computerSelection){
+            roundMessage = 'Tie';
+        }else if(playerSelection == 'rock'){
+            if(computerSelection == 'paper'){
+                computerScore++;
+                roundMessage = 'You lose! Paper beats Rock!';
+            }else{
+                playerScore++;
+                roundMessage = 'You won! Rock was the best choice!';
+            }
+        }else if(playerSelection == 'scissors'){
+            if(computerSelection == 'rock'){
+                computerScore++;
+                roundMessage = 'You lost! Rock beats Scissors!';
+            }else{
+                playerScore++;
+                roundMessage = 'You won! Scissors was the best choice!';
+            }
+        }else if(playerSelection == 'paper'){
+            if(computerSelection == 'scissors'){
+                computerScore++;
+                roundMessage = 'You lose! Scissors beats Paper!';
+            }else{
+                playerScore++;
+                roundMessage = 'You won! Paper was the best choice!';
+            }
         }
     }
 
+    
+    //playRound(playerSelection, computerSelection);
+    //console.log(roundMessage)
+    function game(){
+        for(let i = 0; i < 5; i++){
+
+            playerSelection = prompt("Please choose from the folloing: Paper, Rock, or Scissors.");
+
+            if(roundMessage = 'tie'){
+                playRound(playerSelection, computerSelection);
+            }else{
+                return roundMessage;
+            }
+
+            //playRound(playerSelection, computerSelection);
+            // console.log(computerScore, playerScore)
+            // console.log(roundMessage)
+        }
+    }
+
+    // Calling the game fucntion
+    game();
+
 }
 
-const playerSelection = 'rock';
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+// Calling the playGame function
+playGame();
+
+
+
+
+
+
+
