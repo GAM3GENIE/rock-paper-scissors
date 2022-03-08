110fc37fa56ea9b566c6f9b828079714c84acf89
GAM3GENIE <davidlbernardino@gmail.com>
2022-03-08 08:28:14 -0800

    input user prompt logic

--- app.js
+++ app.js
@@ -2,57 +2,78 @@ const game = () => {
 let roundMessage = '';
 let playerScore = 0;
 let computerScore = 0;
-let playerSelection = prompt('Please choose from Rock, Paper, and Scissors.');
+let gameRound = 0;
+let playerSelection = '';
+//let playerSelection = prompt('Please choose from Rock, Paper, and Scissors.').toLowerCase();
 
-const compTurn = ['rock', 'paper', 'scissors'];
+    const compTurn = ['rock', 'paper', 'scissors'];
 
-    const computerPlay = (compTurn) => {
-        return compTurn[Math.floor(Math.random() * compTurn.length)];
-    }
+            const computerPlay = (compTurn) => {
+                return compTurn[Math.floor(Math.random() * compTurn.length)];
+            }
 
-    const computerSelection = computerPlay(compTurn);
-    //console.log(computerSelection);
+            const computerSelection = computerPlay(compTurn);
+            //console.log(computerSelection);
 
-    const playRound = (playerSelection, computerSelection) => {
-        switch(playerSelection + computerSelection){
-            case 'scissorspaper':
-            case 'rockscissors':
-            case 'paperrock':
-                roundMessage = 'You chose ' + playerSelection + ' and the Computer chose ' + computerSelection + ' , You Win!';
-                playerScore++;
-                break;
-            case 'paperscissors':
-            case 'scissorsrock':
-            case 'rockpaper':
-                roundMessage = 'You chose ' + playerSelection + ' and the Computer chose ' + computerSelection + ' , You Lose!';
-                computerScore++;
-                break;
-            case 'paperpaper':
-            case 'rockrock':
-            case 'scissorsscissors':
-                roundMessage = 'You chose ' + playerSelection + ' and the Computer chose ' + computerSelection + ' , Its a Draw!';
-                break;
-        }
-    }
-    
+            //playRound(playerSelection, computerSelection);
+            //console.log(roundMessage)
+            const playGame = () => {
+                for(let i = 1; i <= 5; i++){
+                    
+                    promptInput(playerSelection);
+                    playRound(playerSelection, computerSelection);
 
-    
-    //playRound(playerSelection, computerSelection);
-    //console.log(roundMessage)
-    const playGame = () => {
-        for(let i = 0; i < 5; i++){
+                    console.log('Computer picked ' + computerSelection);
+                    console.log('You picked ' + playerSelection);
+                    console.log('Computer score is ' + computerScore);
+                    console.log('Your score is ' + playerScore);
+                    console.log('Game round is ' + gameRound);
+                    console.log(roundMessage);
+                    console.log('***********');
+                }
+                // if(score[playerScore] > score[computerScore]){
+                //     return 'You Won! ${score[playerscore]} - ${score[computer score]}';
+                // }else{
+                //     return 'You Lost! ${score[playerscore]} - ${score[computer score]}';
+                // }
+            }
+
+            const promptInput = () => {
+                let playerSelection = prompt('Please choose from Rock, Paper, and Scissors.').toLowerCase();
+                if(playerSelection == 'scissors' || playerSelection == 'paper' || playerSelection == 'rock'){
+                    return playerSelection;
+                }else{
+                    return promptInput = prompt('That is not a legal selection, please choose from Rock, Paper, or Scissors.');
+                    
+                }
+            }
+
+            const playRound = (playerSelection, computerSelection) => {
+                switch(playerSelection + computerSelection){
+                    case 'scissorspaper':
+                    case 'rockscissors':
+                    case 'paperrock':
+                        roundMessage = 'You chose ' + playerSelection + ' and the Computer chose ' + computerSelection + ' , You Win!';
+                        playerScore++;
+                        gameRound++;
+                        break;
+                    case 'paperscissors':
+                    case 'scissorsrock':
+                    case 'rockpaper':
+                        roundMessage = 'You chose ' + playerSelection + ' and the Computer chose ' + computerSelection + ' , You Lose!';
+                        computerScore++;
+                        gameRound++;
+                        break;
+                    case 'paperpaper':
+                    case 'rockrock':
+                    case 'scissorsscissors':
+                        roundMessage = 'You chose ' + playerSelection + ' and the Computer chose ' + computerSelection + ' , Its a Draw!';
+                        break;
+                }
+            }
             
-            //playerSelection = prompt("Please choose from the folloing: Paper, Rock, or Scissors.").toLowerCase();
-
-            playRound(playerSelection, computerSelection);
-            console.log(computerSelection);
-            console.log(computerScore, playerScore);
-            console.log(roundMessage);
-        }
-    }
-
-    // Calling the playGame fucntion
-    playGame();
+            // Calling the playGame fucntion
+            playGame();
 
 }
 
